c0442f9f5479845680b94a1d6b45eeece604a8cb
Tony Mihut <antonio.mihut@endava.com>
2019-01-16 17:55:39 +0200

    Added some comments

--- promises.js
+++ promises.js
@@ -1,6 +1,15 @@
 
-// Allow a prefered outcome to be passed from the command line
+/**
+* Allow an outcome to be passed from the command line
+*/
 const outcome = process.argv[ 2 ] || 'success'
+
+
+/**
+* Create a new promise object
+* @param {function} [resolve] Callback function for success outcome
+* @param {function} [reject] Callback function for fail outcome
+*/
 
 const doSomeAsyncWork = new Promise( ( resolve, reject ) => {
   console.log( `Loading...` )
