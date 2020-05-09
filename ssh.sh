7b0361a294579c328c284d85d3a5486684326c87
Tony Mihut <tonymihut@gmail.com>
2020-05-09 14:58:43 +0300

    Update ssh

--- ssh.sh
+++ ssh.sh
@@ -2,5 +2,5 @@
 ssh-keygen -t rsa -b 4096 -C "example@email.com"
 
 # Add the jey to agent
-eval "$(ssh-agent -s)"
+eval "ssh-agent"
 ssh-add ../path/to/key.rsa # adding -K flag will store it on the Apple keychain
