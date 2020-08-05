1bdd1b0d623cd069b6744b001cba22ffe37c7ee0
Tony Mihut <tonymihut@gmail.com>
2020-08-05 13:49:07 +0300

    Add config edit command

--- git.sh
+++ git.sh
@@ -50,3 +50,4 @@ git config user.email me@awesome.worldwide # Set email address to use on commits
 
 ## Global
 git config --global core.editor "code --wait" # Set vscode as default editor used by git, substitute "code" for preffered editor but keep in mind the value used must be present in PATH otherwise it will default to VIM!! OMG! :q enter
+git config --global --edit ## Open global settings in editor
